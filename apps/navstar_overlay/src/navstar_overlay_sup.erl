1b486fdf071d7d491cdfb56bdcd583319bf61cd7
Deepak Goel <dgoel@mesosphere.com>
2016-10-07 22:03:33 -0700

    Patch to:    1. refactor navstar overlay code    2. Batch the lashup events before applying

--- apps/navstar_overlay/src/navstar_overlay_sup.erl
+++ apps/navstar_overlay/src/navstar_overlay_sup.erl
@@ -33,6 +33,7 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, {{one_for_all, 5, 10}, [
-        ?CHILD(navstar_overlay_poller, worker)
+    {ok, {{rest_for_one, 5, 10}, [
+        ?CHILD(navstar_overlay_poller, worker),
+        ?CHILD(navstar_overlay_lashup_kv_listener, worker)
     ]}}.
