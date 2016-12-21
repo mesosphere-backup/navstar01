e5bb79fc25881969c65a9dc203aea0fc750a3e58
Deepak Goel <dgoel@mesosphere.com>
2016-12-20 16:31:10 -0800

    Adds POST and test to navstar rest

--- apps/navstar_rest/src/navstar_rest_dns_handler.erl
+++ apps/navstar_rest/src/navstar_rest_dns_handler.erl
@@ -14,8 +14,10 @@
 
 %% API
 -export([init/2]).
--export([content_types_provided/2, allowed_methods/2]).
--export([to_json/2]).
+-export([content_types_provided/2, allowed_methods/2, content_types_accepted/2]).
+-export([to_json/2, update_zone/2]).
+
+-define(RECORDS_FIELD, {records, riak_dt_orswot}).
 
 init(Req, Opts) ->
     {cowboy_rest, Req, Opts}.
@@ -26,14 +28,81 @@ content_types_provided(Req, State) ->
     ], Req, State}.
 
 allowed_methods(Req, State) ->
-    {[<<"GET">>], Req, State}.
+    {[<<"GET">>, <<"POST">>], Req, State}.
+
+content_types_accepted(Req, State) ->
+    {[
+        {<<"application/json">>, update_zone}
+    ], Req, State}.
+
+update_zone(Req, State) ->
+    <<"POST">> = cowboy_req:method(Req),
+    true = cowboy_req:has_body(Req),
+    [ZoneName] = cowboy_req:path_info(Req),
+    {ok, Body, _Req2} = cowboy_req:body(Req),
+    Result = case cowboy_req:header(<<"clock">>, Req, <<>>) of
+               <<>> ->
+                   update_zone(ZoneName, [], Body);
+               Clock0 ->
+                   Clock1 = binary_to_term(base64:decode(Clock0)),
+                   update_zone(ZoneName, Clock1, Body)
+               end,
+    Req2 = case Result of
+             {error, Reason} ->
+                 cowboy_req:reply(400, [], Reason, Req);
+             _ ->
+                 cowboy_req:reply(200, Req)
+           end,
+    {<<>>, Req2, State}.
+
+update_zone(ZoneName, Clock, Body0) ->
+    Body1 = jsx:decode(Body0, [return_maps, {labels, atom}]),
+    Records = maps:get(records, Body1),
+    ParsedRecords = lists:map(fun map_to_record/1, Records),
+    push_zone_to_lashup(ZoneName, Clock, ParsedRecords).
+
+push_zone_to_lashup(ZoneName, Clock, NewRecords) ->
+    ZoneKey = [navstar, dns, zones, ZoneName],
+    {OriginalMap, VClock} = lashup_kv:value2(ZoneKey),
+    case {Clock, VClock} of
+        {_, []} ->
+           push_zone_to_lashup(ZoneKey, Clock, [], NewRecords);
+        {[], _} -> {error, "To modify existing record, Clock header is required"};
+        {_, _} ->
+           {_, OldRecords0} = lists:keyfind(?RECORDS_FIELD, 1, OriginalMap),
+           OldRecords1 = lists:usort(OldRecords0),
+           push_zone_to_lashup(ZoneKey, Clock, OldRecords1, NewRecords)
+    end.
+
+push_zone_to_lashup(ZoneKey, Clock, OldRecords, NewRecords) ->
+    case ops(OldRecords, NewRecords) of
+        [] ->
+            {ok, no_change};
+        Ops ->
+            lashup_kv:request_op(ZoneKey, Clock, {update, Ops})
+    end.
+
+ops(OldRecords, NewRecords) ->
+    RecordsToDelete = ordsets:subtract(OldRecords, NewRecords),
+    RecordsToAdd = ordsets:subtract(NewRecords, OldRecords),
+    Ops0 = lists:foldl(fun delete_op/2, [], RecordsToDelete),
+    case RecordsToAdd of
+        [] ->
+            Ops0;
+        _ ->
+            [{update, ?RECORDS_FIELD, {add_all, RecordsToAdd}}|Ops0]
+    end.
+
+delete_op(Record, Acc0) ->
+    Op = {update, ?RECORDS_FIELD, {remove, Record}},
+    [Op|Acc0].
 
 to_json(Req, State) ->
     [Zone] = cowboy_req:path_info(Req),
     fetch_zone(Zone, Req, State).
 
 fetch_zone(Zone, Req, State) ->
-    {Value0, Clock} = lashup_kv:value2([navstar,dns,zones,Zone]),
+    {Value0, Clock} = lashup_kv:value2([navstar, dns, zones, Zone]),
     [Value1] = lists:map(fun encode_value/1, Value0),
     Body = jsx:encode(Value1),
     Req2 = cowboy_req:reply(200, [
@@ -48,7 +117,7 @@ encode_value2(Record0 = #dns_rr{data = Data}) ->
     DataMap = record_to_map(Data),
     Record1 = Record0#dns_rr{data = DataMap},
     record_to_map(Record1).
-    
+
 record_to_map(Record = #dns_rrdata_a{}) ->
     maps:from_list(lists:zip(record_info(fields, dns_rrdata_a), tl(tuple_to_list(Record))));
 record_to_map(Record = #dns_rrdata_ns{}) ->
@@ -59,3 +128,20 @@ record_to_map(Record = #dns_rrdata_srv{}) ->
     maps:from_list(lists:zip(record_info(fields, dns_rrdata_srv), tl(tuple_to_list(Record))));
 record_to_map(Record = #dns_rr{}) ->
     maps:from_list(lists:zip(record_info(fields, dns_rr), tl(tuple_to_list(Record)))).
+
+map_to_record(#{class := Class, name := Name, ttl := TTL, type := Type, data := Data}) ->
+    DataRecord = map_to_record(Type, Data),
+    #dns_rr{class = Class, name = Name, ttl = TTL, type = Type, data = DataRecord}.
+
+map_to_record(?DNS_TYPE_A, #{ip := IP}) when is_binary(IP)->
+    IPStr = binary_to_list(IP),
+    {ok, ParsedIP} = inet:parse_ipv4_address(IPStr),
+    #dns_rrdata_a{ip = ParsedIP};
+map_to_record(?DNS_TYPE_NS, #{dname := Dname}) ->
+    #dns_rrdata_ns{dname = Dname};
+map_to_record(?DNS_TYPE_SOA, #{mname := Mname, rname := Rname, serial := Serial, refresh := Refresh, retry := Retry,
+               expire := Expire, minimum := Minimum}) ->
+    #dns_rrdata_soa{mname = Mname, rname = Rname, serial = Serial, refresh = Refresh, retry = Retry, expire = Expire,
+                    minimum = Minimum};
+map_to_record(?DNS_TYPE_SRV, #{priority := Priority, weight := Weight, port := Port, target := Target}) ->
+    #dns_rrdata_srv{priority = Priority, weight = Weight, port = Port, target = Target}.
--- apps/navstar_rest/test/navstar_rest_SUITE.erl
+++ apps/navstar_rest/test/navstar_rest_SUITE.erl
@@ -11,23 +11,63 @@
 
 -export([all/0]).
 
--export([navstar_rest_dns_test/1]).
+-export([navstar_rest_dns_get_test/1, navstar_rest_dns_post_test/1, navstar_rest_dns_modify_test/1]).
 
 -include_lib("dns/include/dns.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
 
 -define(RECORDS_FIELD, {records, riak_dt_orswot}).
 
-all() -> [navstar_rest_dns_test].
+all() -> [navstar_rest_dns_get_test, navstar_rest_dns_post_test, navstar_rest_dns_modify_test].
 
-navstar_rest_dns_test(_Config) ->
+navstar_rest_dns_get_test(_Config) ->
     application:ensure_all_started(navstar_rest),
     ZoneName = <<"dcos.thisdcos.directory">>,
     Records = base_records(ZoneName),
     push_zone_to_lashup(ZoneName, Records),   
     URI = lists:flatten(io_lib:format("http://127.0.0.1:62080/dns/~s", [binary_to_list(ZoneName)])),
-    {ok, {_Status, _Headers, Body}} = httpc:request(get, {URI, []}, [], [{body_format, binary}]),
+    {ok, {_Status, _Headers0, Body}} = httpc:request(get, {URI, []}, [], [{body_format, binary}]),
     DecodedBody = jsx:decode(Body),
+    io:format("DecodedBody: ~p~n", [DecodedBody]),
     [{<<"records">>, [_ | _]}] = DecodedBody,
+    ok.
+
+navstar_rest_dns_post_test(_Config) ->
+    application:ensure_all_started(navstar_rest),
+    ZoneName = <<"test.thisdcos.directory">>,
+    Record = #{class => 1, data => #{ip => <<"9.0.0.1">>}, name => ZoneName, ttl => 3600, type => 1},
+    Records = #{records => [Record]},
+    Body = jsx:encode(Records),
+    URI = lists:flatten(io_lib:format("http://127.0.0.1:62080/dns/~s", [binary_to_list(ZoneName)])),
+    {ok, _} = httpc:request(post, {URI, [], "application/json", Body}, [], []),
+    Value = lashup_kv:value([navstar, dns, zones, ZoneName]),
+    io:format("Value ~p~n", [Value]),
+    [{{records, riak_dt_orswot}, [_]}] = Value,
+    ok.
+
+navstar_rest_dns_modify_test(_Config) ->
+    application:ensure_all_started(navstar_rest),
+    ZoneName = <<"dcos.thisdcos.directory">>,
+    URI = lists:flatten(io_lib:format("http://127.0.0.1:62080/dns/~s", [binary_to_list(ZoneName)])),
+
+    %% fetch current records
+    {ok, {_Status, Headers, Body}} = httpc:request(get, {URI, []}, [], [{body_format, binary}]),
+    RecordMap = jsx:decode(Body, [return_maps, {labels, atom}]),
+    OldRecords = maps:get(records, RecordMap),
+    [_,_] = OldRecords,
+
+    %% modify records
+    NewRecord = #{class => 1, data => #{ip => <<"9.0.0.1">>}, name => ZoneName, ttl => 3600, type => 1},
+    AddedRecords = ordsets:union(ordsets:from_list(OldRecords), ordsets:from_list([NewRecord])),
+    NewRecordMap = maps:put(records, AddedRecords, maps:new()),
+    PostBody = jsx:encode(NewRecordMap),
+    {ok, _} = httpc:request(post, {URI, Headers, "application/json", PostBody}, [], []),
+
+    %% verify modified records
+    Value = lashup_kv:value([navstar, dns, zones, ZoneName]),
+    io:format("Final Records ~p~n", [Value]),
+    [{{records, riak_dt_orswot}, [_,_,_]}] = Value,
     ok.
 
 base_records(ZoneName) ->
