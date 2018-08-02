10fdc9ad1c8964edaf1a3dd5706d749b755ce305
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-08-02 15:55:44 -0700

    Add killer for stuck processes

--- apps/navstar/src/navstar_sup.erl
+++ apps/navstar/src/navstar_sup.erl
@@ -29,7 +29,7 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    {ok, { {one_for_all, 10000, 1}, [?CHILD(navstar_killer, worker)]} }.
 
 %%====================================================================
 %% Internal functions
